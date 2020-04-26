ec077c9bf743f5b70ea7a7a0957935db2793abb0
Skip Khamvongsa <skipk95@gmail.com>
2020-04-26 17:44:10 -0600

    ADded modal, and function to match

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,34 @@ var peopleArray = [
         "scores":[
             5, 1, 4, 4, 5, 1, 2, 5, 4, 1
         ]
+    },
+    {
+        "name": "Danny",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5, 1, 4, 2, 1, 5, 5, 5, 4, 4
+        ]
+    },
+    {
+        "name": "Jessie",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1, 3, 5, 5, 5, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        "name": "Tarrie",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1, 5, 5, 3, 4, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        "name": "Kelsie",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5, 5, 5, 4, 5, 1, 2, 1, 1, 1
+        ]
     }
 ]
 
