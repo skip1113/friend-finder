556a4370a71b5fae3e30c358ad92ea485f305693
Skip Khamvongsa <skipk95@gmail.com>
2020-04-25 21:56:37 -0600

    Edited api route and html route

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-var ahmed = [
+var peopleArray = [
     {
         "name": "Ahmed",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -7,3 +7,5 @@ var ahmed = [
         ]
     }
 ]
+
+module.exports = peopleArray;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,16 +9,16 @@ module.exports = function(app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  
+  //Route: Home page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
-
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  //Route: Survey
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-
+  
   // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
